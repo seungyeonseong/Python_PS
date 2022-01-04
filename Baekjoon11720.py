f3002918be014248f052a7a896c23885596f0c40
승엥 <96861618+seungyeonseong@users.noreply.github.com>
2022-01-04 18:36:52 +0900

    Add files via upload

---
+++ Baekjoon11720.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+n = input()
+#ord(): 특정한 한 문자를 아스키 코드값으로 변환
+print(ord(n))
+
+# +
+n = int(input())
+num = int(input())
+total = 0
+while num >= 10:
+    total += num%10
+    num = num//10
+total += num
+print(total)
+    
+    
+
+# -
+
+
