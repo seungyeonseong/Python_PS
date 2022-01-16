3fbc56257353330e771c91a434e14d6e88d85ece
승엥 <96861618+seungyeonseong@users.noreply.github.com>
2022-01-16 17:26:17 +0900

    Add files via upload

---
+++ Baekjoon3053.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+
+import math
+
+r = int(input())
+pie=math.pi
+
+print("{:.6f}".format(pie*r*r))
+print("{:.6f}".format(2*r*r))
+
+
+
+
+
+
+
+
+
+
+# -
+
+
