b432236920173993abeb2b967af479ba94b449d1
승엥 <96861618+seungyeonseong@users.noreply.github.com>
2022-01-17 13:31:22 +0900

    Add files via upload

---
+++ Baekjoon10872.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+n = int(input())
+
+def fac(n):
+    if n==0:
+        return 1
+    return n*fac(n-1)
+
+print(fac(n))
+# -
+
+
+
