51d1e953c761422e1696aa2dd31ae05000ec805a
승엥 <96861618+seungyeonseong@users.noreply.github.com>
2022-01-22 11:11:20 +0900

    Add files via upload

---
+++ Baekjoon2475.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+li = list(map(int,input().split()))
+result =0
+for i in li:
+    result += i**2
+print(result%10)
+
+
