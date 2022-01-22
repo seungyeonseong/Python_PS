41675340a5a3794d58602e43a7fb80e37edae2cc
승엥 <96861618+seungyeonseong@users.noreply.github.com>
2022-01-22 11:08:37 +0900

    Add files via upload

---
+++ Baekjoon2558.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+n = int(input())
+li=[]
+for i in range(n):
+    li.append(int(input()))
+              
+li.sort()
+              
+for j in li:
+    print(j)
+# -
+
+
+A = int(input())
+B = int(input())
+print(A+B)
+
+
