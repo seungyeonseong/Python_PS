4363a720b29ccbcccb6901773dba4ac43578f374
승엥 <96861618+seungyeonseong@users.noreply.github.com>
2022-01-26 13:49:03 +0900

    Add files via upload

---
+++ Baekjoon18108.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+y = int(input())
+#불기2541->서기1998
+print(y+(-2541+1998))
+
+
