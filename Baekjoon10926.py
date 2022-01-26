4b881a229fb0ff9c8b435309b10682948e2b7910
승엥 <96861618+seungyeonseong@users.noreply.github.com>
2022-01-26 13:46:21 +0900

    Add files via upload

---
+++ Baekjoon10926.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+
+id = input()
+print(id+'??!')
+# -
+
+
+
