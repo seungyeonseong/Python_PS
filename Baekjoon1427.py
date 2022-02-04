1e346d98f40fa76f179a29656fefa11ebeebfee0
승엥 <96861618+seungyeonseong@users.noreply.github.com>
2022-02-04 20:02:38 +0900

    Add files via upload

---
+++ Baekjoon1427.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+import sys
+input = sys.stdin.readline
+n = list(input())
+n.sort(reverse = True)
+for i in n:
+    print(i,end="")
+
+
+
