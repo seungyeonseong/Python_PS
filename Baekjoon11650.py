3996c8f038860dd824461d0a38b00d34ebdec720
승엥 <96861618+seungyeonseong@users.noreply.github.com>
2022-02-04 20:06:48 +0900

    Add files via upload

---
+++ Baekjoon11650.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+li=[]
+for i in range(n):
+    x,y = map(int,input().split())
+    li.append((x,y))
+li.sort(key=lambda x:(x[0],x[1]))
+for i in li:
+    print(i[0],i[1])
+# -
+
+
