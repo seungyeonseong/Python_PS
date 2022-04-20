8688906b3aaaf0b8c6c27ba2769f43d668a3f398
승엥 <96861618+seungyeonseong@users.noreply.github.com>
2022-04-20 12:26:58 +0900

    Add files via upload

---
+++ Programmers_내적.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+def solution(a, b):
+    answer =0
+    for x,y in zip(a,b):
+        answer += x*y
+    return answer
