a2123f68dd0d54cc7377bc0aca31424264296ec9
승엥 <96861618+seungyeonseong@users.noreply.github.com>
2022-07-04 17:02:42 +0900

    Add files via upload

---
+++ Baekjoon1436.py
@@ -0,0 +1,25 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+N = int(input())
+movie = 666
+
+while N:
+    if "666" in str(movie):
+        N -= 1
+    movie += 1
+
+print(movie - 1)
