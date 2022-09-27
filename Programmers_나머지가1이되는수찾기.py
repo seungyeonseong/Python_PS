9be91409abe9b2bff5b95c8b00d6e1f811db3600
승엥 <96861618+seungyeonseong@users.noreply.github.com>
2022-09-27 13:06:54 +0900

    Add files via upload

---
+++ Programmers_나머지가1이되는수찾기.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+def solution(n):
+    answer = n-1
+    for i in range(2,n):
+        if n%i==1:
+            answer = min(answer,i)
+
+    return answer
