0eea102b50fc0afa75b7f5eea7df0143e942f345
승엥 <96861618+seungyeonseong@users.noreply.github.com>
2022-09-27 13:14:37 +0900

    Add files via upload

---
+++ Programmers_부족한금액계산하기.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+def solution(price, money, count):
+    answer = price*count*(count+1)//2
+    if money < answer:
+        return answer-money
+    else:
+        return 0
