2eeeeb5f1c589c6568516f158aefa3d18ce273d4
seungyeonseong <96861618+seungyeonseong@users.noreply.github.com>
2022-10-01 21:14:14 +0900

    백준11403

--- Baekjoon11403.py
+++ Baekjoon11403.py
@@ -14,15 +14,3 @@ for k in range(n):
                 graph[i][j] = 1
 print(graph)
 
-
-
-
-# for i in range(n):
-#     for j in range(n):
-#         if graph[i][j]==1:
-#             bfs(i,j)
-
-
-
-
-
