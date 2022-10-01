27e4eede862f7e9efa1ce5ea4acadd247e2ee618
seungyeonseong <96861618+seungyeonseong@users.noreply.github.com>
2022-10-01 21:11:44 +0900

    백준1946

---
+++ Baekjoon11403.py
@@ -0,0 +1,29 @@
+import sys
+
+input =sys.stdin.readline
+
+n = int(input())
+graph = []
+for _ in range(n):
+    graph.append(list(map(int,input().split())))
+
+#플로이드워셜로 경로찾기
+for k in range(n):
+    for i in range(n):
+        for j in range(n):
+            if graph[i][j]==1 or (graph[i][k]==1 and graph[k][j]==1):
+                graph[i][j] = 1
+print(graph)
+
+
+
+
+# for i in range(n):
+#     for j in range(n):
+#         if graph[i][j]==1:
+#             bfs(i,j)
+
+
+
+
+
