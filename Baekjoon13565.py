8697a7f0c37a5e9d3f86d38246ddd3ac3aa065e2
seungyeonseong <96861618+seungyeonseong@users.noreply.github.com>
2022-10-03 00:07:58 +0900

    백준13565

--- Baekjoon13565.py
+++ Baekjoon13565.py
@@ -1,38 +1,39 @@
 import sys
 from collections import deque
+
 input = sys.stdin.readline
 
-dx = [1,0,-1,0]
-dy = [0,1,0,-1]
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
 
-def bfs(x,y,visited):
+
+def bfs(x, y, visited, graph):
     q = deque()
-    q.append((x,y))
+    q.append((x, y))
     visited[x][y] = True
     while q:
-        x,y = q.popleft()
+        x, y = q.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx <m and 0<= ny < n and graph[nx][ny]=="0":
+            if 0 <= nx < m and 0 <= ny < n and graph[nx][ny] == "0" and visited[nx][ny] is False:
                 visited[nx][ny] = True
-                q.append((nx,ny))
-    return
+                q.append((nx, ny))
 
 
-m,n = map(int,input().split())
+m, n = map(int, input().split())
 graph = []
 for _ in range(m):
     graph.append(list(input()))
-visited = [[False]*n for _ in range(m)]
+visited = [[False] * n for _ in range(m)]
 
 for i in range(n):
-    if graph[0][i]=="0":
-        bfs(0,i,visited)
+    if graph[0][i] == "0":
+        bfs(0, i, visited, graph)
+
 answer = "NO"
 for i in range(n):
-    if visited[m-1][i] is True:
+    if visited[m - 1][i] is True:
         answer = "YES"
         break
 print(answer)
-print(visited)
