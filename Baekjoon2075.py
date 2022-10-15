515e50014ca342156ffee1b1b0c4e7bd3f405051
seungyeonseong <96861618+seungyeonseong@users.noreply.github.com>
2022-10-15 23:54:55 +0900

    백준_우선순위큐

--- Baekjoon2075.py
+++ Baekjoon2075.py
@@ -1,6 +1,5 @@
 #graph를 따로 만들었더니 메모리초과
 #tmp보다 큰 값만 push했더니 시간초과 -> 큐 개수로 수정
-
 import heapq
 import sys
 input = sys.stdin.readline
@@ -16,6 +15,5 @@ for _ in range(n):
             if q[0] < num:
                 heapq.heappop(q)
                 heapq.heappush(q,num)
-
 print(q[0])
 
