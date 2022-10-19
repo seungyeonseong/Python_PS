0d1cda75508090cb1a01b72c852c8ee7ada23a04
seungyeonseong <96861618+seungyeonseong@users.noreply.github.com>
2022-10-19 19:14:33 +0900

    백준_트리

---
+++ Baekjoon1068.py
@@ -0,0 +1,25 @@
+# n = 5
+# parent= [-1,0,0,1,1]
+# rm = 1
+import sys
+input = sys.stdin.readline
+n = int(input())
+parent = list(map(int,input().split()))
+rm = int(input())
+
+def dfs(rm,li):
+    li[rm] = -2
+    for idx,i in enumerate(li):
+        if i ==rm:
+            dfs(idx,li)
+
+dfs(rm,parent)
+cnt = 0
+for i in range(n):
+    if parent[i] != -2 and i not in parent:
+        cnt += 1
+print(cnt)
+print(parent)
+
+
+
