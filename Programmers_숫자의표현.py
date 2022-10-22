ba9d6eddf499ffcb863ea134b5e423b859eba03d
seungyeonseong <96861618+seungyeonseong@users.noreply.github.com>
2022-10-22 15:50:08 +0900

    프로그래머스Lv.2

---
+++ Programmers_숫자의표현.py
@@ -0,0 +1,13 @@
+
+def solution(n):
+    answer = 1
+    li = [i for i in range(n+1)]
+    for i in range(1,n):
+        tmp = li[i]
+        idx = i
+        while tmp <= n:
+            if tmp == n:
+                answer += 1
+            tmp += li[idx+1]
+            idx += 1
+    return answer
